039a64a479586f8288009ed9734f7bbc734f570d
patience <patiencewanjiru123@gmail.com>
2023-03-22 12:42:22 +0300

    Completed assignment

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+//Testing
+
+function calculateArea(radius) {
+    const pi = 3.14;
+    let area = pi*radius*radius;
+    return area;
+}
+
+let circleArea = calculateArea(5);
+console.log(circleArea);
